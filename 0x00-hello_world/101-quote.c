29a3d032c755ad470266d87bdbbe7e835d98b525
Victor Munene <munenevictor39.vm@gmail.com>
2023-09-11 10:41:08 -0700

    101quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <unistd.h>
-#include <unisted.h>
+#include <unistd.h>
 /**
  * main - main function
  * Return: 1
@@ -7,6 +7,6 @@
 
 int main(void)
 {
-	write 2), "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
