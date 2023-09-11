a331ef21638a87c0335eaa801dd47ceea5880ff6
Victor Munene <munenevictor39.vm@gmail.com>
2023-09-11 10:13:42 -0700

    @

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <unisted.h>
+/**
+ * main - main function
+ * Return: 1
+ */
+
+int main(void)
+{
+	write 2), "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	return (1);
+}
