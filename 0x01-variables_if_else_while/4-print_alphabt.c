3c7958fb0276d4b682733e3cdfccf3ee6bf5d1db
Victor Munene <munenevictor39.vm@gmail.com>
2023-09-12 03:43:32 -0700

    4prntcorr

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,21 +3,25 @@
 /**
  * main - Entry point of the program
  *
+ * Description: function prints lowercase letters 'a' to 'z' from 'a' to 'z'
+ * except for 'e' and 'q', followed newline charactr
  *
  * Return: Always 0 (Success)
  */
+
 
 int main(void)
 
 {
 	char letter = 'a';
+
 	while (letter <= 'z')
 	{
-        if (letter != 'e' && letter != 'q')
-        {
-		putchar(letter);
-        }
-        letter++;
+	if (letter != 'e' && letter != 'q')
+	{
+	putchar(letter);
+	}
+	letter++;
 	}
 	putchar('\n');
 	return (0);
