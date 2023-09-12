3adb93ab3ef1068a5a19d557e9495ca62e4cfa7b
Victor Munene <munenevictor39.vm@gmail.com>
2023-09-12 01:10:30 -0700

    5print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * 
+ *
+ *
+ *
+ * Return: Always (Success)
+ */
+int main(void) 
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i + '0');
+	}
+	
+	putchar('\n');
+	return (0);
+}
