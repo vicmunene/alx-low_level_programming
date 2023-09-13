3f2d0dc48fd0b3d6ba92c595933683c246b7ce8f
Victor Munene <munenevictor39.vm@gmail.com>
2023-09-12 20:20:47 -0700

    puchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+/**
+ * main - Entry point
+ *
+ * Description: This is a simple C program that prints "_putchar"
+ *              followed by a new line and returns 0.
+ *
+ * Return: Always 0 (Success)
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
