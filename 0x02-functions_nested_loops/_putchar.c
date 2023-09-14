2efee9f520e3e944ea1e79a6034380f017ebdbf6
Victor Munene <munenevictor39.vm@gmail.com>
2023-09-13 19:43:44 -0700

    1apha

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,1 +1,13 @@
-#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - Writes a character to the standard output
+ * @c: The character to be written
+ *
+ * Return: On success, the character is returned. On error, -1 is returned.
+ */
+int _putchar(char c)
+
+{
+	return (write(1, &c, 1));
+}
