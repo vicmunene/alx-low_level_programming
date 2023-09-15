cca3b068281e749a627a5d6c2e7054ac39cd7c3e
Victor Munene <munenevictor39.vm@gmail.com>
2023-09-14 20:21:09 -0700

    2printfinally

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,13 @@ void print_alphabet_x10(void)
 	char c;
 	int i=0;
 
-	while (i<=0)
+	while (i<=9)
 	{
-		for (c = 'a'; c <= 'z'; i++)
+		for (c = 'a'; c <= 'z'; c++)
 	{
 		_putchar(c);
 	}
 		_putchar('\n');
+		i++;
 	}
 }
