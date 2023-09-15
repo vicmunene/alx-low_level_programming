9d3162cbb5725d552b42e6efae3485b9cc8feffc
Victor Munene <munenevictor39.vm@gmail.com>
2023-09-14 19:54:56 -0700

    printstub

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the lowercase alphabet 10 times.
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i=0;
+
+	while (i<=0)
+	{
+		for (c = 'a'; c <= 'z'; i++)
+	{
+		_putchar(c);
+	}
+		_putchar('\n');
+	}
+}
