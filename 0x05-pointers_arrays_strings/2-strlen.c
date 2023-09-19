b7641af02cf65e16206e08d40616dcf7dbe039b7
Victor Munene <munenevictor39.vm@gmail.com>
2023-09-18 19:44:44 -0700

    2strlencorrr

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,7 +4,9 @@
  * _strlen - Returns the length of a string.
  * @str: The string to be measured.
  * Return: The length of the string.
- */size_t  _strlen(const char *str)
+ */
+size_t  _strlen(const char *str)
+
 {
 	size_t length = 0;
 
@@ -12,5 +14,5 @@
 	{
 	length++;
 	}
-	return length;
+	return (length);
 }
