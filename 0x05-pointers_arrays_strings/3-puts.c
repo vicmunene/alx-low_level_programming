6a528d015fdfa7662088d3702cd41b0601d6acd5
Victor Munene <munenevictor39.vm@gmail.com>
2023-09-18 19:59:01 -0700

    asimov

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * _puts - Prints a string followed by a new line to stdout.
+ * @str: The string to be printed.
+ * Return: void
+ */
+
+void _puts(char *str)
+
+{
+	for (;*str != '\0'; str++)
+	{
+	putchar(*str);
+	}
+	putchar('\n');
+}
