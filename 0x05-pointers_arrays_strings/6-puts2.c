b66cb29d0ac09c49abec09e5777ea9c3b264fe28
Victor Munene <munenevictor39.vm@gmail.com>
2023-09-20 00:05:39 -0700

    nganga

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -4,16 +4,21 @@
 /**
  * puts2 - Prints every other character of a string
  * @str: The string to be printed
+ * Return: void
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
+	int j = 0;
 
-	while (str[i] != '\0')
+	while (str[j] != '\0')
 	{
-	putchar(str[i]);
-	i += 2;
+	j++;
 	}
-
+	
+	for (i = 0; i < j; i += 2 )
+	{
+	_putchar(str[i]);
+	}
 	putchar('\n');
 }
