f4f61e0346c3c8f2de9bdd62c786c206ff05b3c9
Victor Munene <munenevictor39.vm@gmail.com>
2023-09-19 20:06:18 -0700

    haraka

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse followed by a new line
+ * @s: The string to be printed in reverse
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+	c++;
+	}
+
+	for (c -=1; c >= 0; c--)
+	{
+	_putchar(s[c]);
+	}
+	_putchar('\n');
+}
