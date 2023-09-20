982b6a798ca4a88bf5cd658dfba12ee2b65f8d1c
Victor Munene <munenevictor39.vm@gmail.com>
2023-09-20 01:35:16 -0700

    9corr

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,20 +4,15 @@
  * _strcpy - Copies a string including the null byte to a buffer
  * @dest: The destination buffer
  * @src: The source string
- *
  * Return: The pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
-
-	while (src[i] != '\0')
-	{
-	dest[i] = src[i];
-	i++;
-	}
-
-	dest[i] = '\0';
-
-	(return dest);
+int i;
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+dest[i++] = '\0';
+return (dest);
 }
